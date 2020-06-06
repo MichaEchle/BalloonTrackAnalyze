2acabba44b392cd6463e991ddb2ec5042487f9b3
Micha Echle <micha_echle@outlook.de>
2020-06-06 19:29:43 +0200

    First data class

---
+++ Coordinates/Coordinates/Coordinate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Coordinates
+{
+    public class Coordinate
+    {
+        public double Longitude { get; private set; }
+
+        public double Latitude { get; private set; }
+
+        public double Altitude { get; private set; }
+
+        public DateTime TimeStamp { get; set; }
+
+    }
+}
