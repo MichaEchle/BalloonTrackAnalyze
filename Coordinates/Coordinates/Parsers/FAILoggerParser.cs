5f9efa9f9891d69d579326e21324322148caff06
MICHA ECHLE <micha_echle@outlook.de>
2023-03-12 11:42:26 -0700

    Fixed exception with parsing the logger interval

--- Coordinates/Coordinates/Parsers/FAILoggerParser.cs
+++ Coordinates/Coordinates/Parsers/FAILoggerParser.cs
@@ -162,7 +162,7 @@ namespace Coordinates.Parsers
                 {
                     string loggerInterval = configLine.Split("=").Last();
                     if (!track.AdditionalPropertiesFromIGCFile.ContainsKey("LoggerInterval"))
-                        track.AdditionalPropertiesFromIGCFile.Add("LoggerInterval", loggerInterval.Remove('s'));
+                        track.AdditionalPropertiesFromIGCFile.Add("LoggerInterval", loggerInterval.Replace("s",""));
                 }
             }
 
