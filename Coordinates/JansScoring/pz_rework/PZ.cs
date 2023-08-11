4932764fb6a7e451b425a0d42ce2177d4ec2e4e4
CodingPhoenixx <jan.f.meinl@web.de>
2023-08-11 15:57:02 +0200

    Added PZs

---
+++ Coordinates/JansScoring/pz_rework/PZ.cs
@@ -0,0 +1,21 @@
+﻿using Coordinates;
+
+namespace JansScoring.pz_rework;
+
+public abstract class PZ
+{
+    /// <summary>
+    /// The ID of the PZ
+    /// </summary>
+    private int id
+    {
+        get;
+    }
+
+    protected PZ(int id)
+    {
+        this.id = id;
+    }
+
+    public abstract bool IsInsidePz(Coordinate coordinate);
+}
---
+++ Coordinates/JansScoring/pz_rework/type/RedPZ.cs
@@ -0,0 +1,15 @@
+﻿using Coordinates;
+
+namespace JansScoring.pz_rework.type;
+
+public class RedPZ : PZ
+{
+    public RedPZ(int id) : base(id)
+    {
+    }
+
+    public override bool IsInsidePz(Coordinate coordinate)
+    {
+        throw new System.NotImplementedException();
+    }
+}
