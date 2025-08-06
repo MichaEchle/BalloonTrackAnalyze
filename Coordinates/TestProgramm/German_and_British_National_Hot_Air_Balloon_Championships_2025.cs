4f2ed2defe1641cc1db7115bd41b3167fd199b90
MICHA ECHLE <micha_echle@keysight.com>
2025-08-06 20:29:55 +0200

    German British championship: Corrected goal T7/8C

--- Coordinates/TestProgramm/German_and_British_National_Hot_Air_Balloon_Championships_2025.cs
+++ Coordinates/TestProgramm/German_and_British_National_Hot_Air_Balloon_Championships_2025.cs
@@ -528,7 +528,7 @@ internal class German_and_British_National_Hot_Air_Balloon_Championships_2025
         {
             ["T7/8A"] = CoordinateHelpers.ConvertUTMToLatitudeLongitudeCoordinate("32U", 623741, 5520990,CoordinateHelpers.ConvertToMeter(864)),
             ["T7/8B"] = CoordinateHelpers.ConvertUTMToLatitudeLongitudeCoordinate("32U", 622470, 5520952, 292),
-            ["T7/8C"] = CoordinateHelpers.ConvertUTMToLatitudeLongitudeCoordinate("32U", 620453, 5520241, CoordinateHelpers.ConvertToMeter(756)),
+            ["T7/8C"] = CoordinateHelpers.ConvertUTMToLatitudeLongitudeCoordinate("32U", 620452, 5520241, CoordinateHelpers.ConvertToMeter(908)),
             ["T7/8D"] = CoordinateHelpers.ConvertUTMToLatitudeLongitudeCoordinate("32U", 622571, 5519901, CoordinateHelpers.ConvertToMeter(925)),
             ["T7/8E"] = CoordinateHelpers.ConvertUTMToLatitudeLongitudeCoordinate("32U", 621436, 5519536, CoordinateHelpers.ConvertToMeter(949)),
             ["T7/8F"] = CoordinateHelpers.ConvertUTMToLatitudeLongitudeCoordinate("32U", 621890, 5518934, CoordinateHelpers.ConvertToMeter(796)),
