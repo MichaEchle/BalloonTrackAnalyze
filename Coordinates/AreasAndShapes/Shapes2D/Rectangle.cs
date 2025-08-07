55c546d5a78c6e5cad429ac3facb906c43e8e2ee
CodingPhoenixx <jan.f.meinl@web.de>
2025-08-07 09:10:14 +0200

    Add Rectangle and Block classes for 2D and 3D boundary checks, remove redundant square boundary method.

---
+++ Coordinates/AreasAndShapes/Shapes2D/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using Coordinates;
+
+namespace Shapes.Shapes2D;
+
+public class Rectangle : Shapes2D
+{
+    public Coordinate FirstBoundary
+    {
+        get; private set;
+    }
+    
+    public Coordinate SecondBoundary
+    {
+        get; private set;
+    }
+
+    public Rectangle(Coordinate firstBoundary, Coordinate secondBoundary)
+    {
+        FirstBoundary = firstBoundary;
+        SecondBoundary = secondBoundary;
+    }
+
+    public override bool IsWithin(Coordinate point)
+    {
+        double minLat = Math.Min(FirstBoundary.Latitude, SecondBoundary.Latitude);
+        double maxLat = Math.Max(FirstBoundary.Latitude, SecondBoundary.Latitude);
+
+        double minLong = Math.Min(FirstBoundary.Longitude, SecondBoundary.Longitude);
+        double maxLong = Math.Max(FirstBoundary.Longitude, SecondBoundary.Longitude);
+
+        bool isLatWithinRange = point.Latitude >= minLat && point.Latitude <= maxLat;
+
+        bool isLongWithinRange = point.Longitude >= minLong && point.Longitude <= maxLong;
+
+        return isLatWithinRange && isLongWithinRange;
+    }
+}
---
+++ Coordinates/AreasAndShapes/Shapes3D/Block.cs
@@ -0,0 +1,46 @@
+﻿using Coordinates;
+
+namespace Shapes.Shapes3D;
+
+public class Block : Shapes3D
+{
+    public Coordinate FirstBoundary
+    {
+        get;
+        private set;
+    }
+
+    public Coordinate SecondBoundary
+    {
+        get;
+        private set;
+    }
+
+    public Block(Coordinate firstBoundary, Coordinate secondBoundary)
+    {
+        FirstBoundary = firstBoundary;
+        SecondBoundary = secondBoundary;
+    }
+
+    public override bool IsWithin(Coordinate point, bool useGPSAltitude)
+    {
+        double minLat = Math.Min(FirstBoundary.Latitude, SecondBoundary.Latitude);
+        double maxLat = Math.Max(FirstBoundary.Latitude, SecondBoundary.Latitude);
+
+        double minLong = Math.Min(FirstBoundary.Longitude, SecondBoundary.Longitude);
+        double maxLong = Math.Max(FirstBoundary.Longitude, SecondBoundary.Longitude);
+
+        double minAltitude = Math.Min((useGPSAltitude ? FirstBoundary.AltitudeGPS : FirstBoundary.AltitudeBarometric),
+            (useGPSAltitude ? SecondBoundary.AltitudeGPS : SecondBoundary.AltitudeBarometric));
+        double maxAltitude = Math.Max((useGPSAltitude ? FirstBoundary.AltitudeGPS : FirstBoundary.AltitudeBarometric),
+            (useGPSAltitude ? SecondBoundary.AltitudeGPS : SecondBoundary.AltitudeBarometric));
+
+        bool isLatWithinRange = point.Latitude >= minLat && point.Latitude <= maxLat;
+
+        bool isLongWithinRange = point.Longitude >= minLong && point.Longitude <= maxLong;
+        
+        bool isAltitudeWithinRange = point.AltitudeGPS >= minAltitude && point.AltitudeGPS <= maxAltitude;
+
+        return isLatWithinRange && isLongWithinRange && isAltitudeWithinRange;
+    }
+}
