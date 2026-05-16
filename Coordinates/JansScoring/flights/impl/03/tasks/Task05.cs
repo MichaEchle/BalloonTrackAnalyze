84b6b105df5ba65c97263fbd207517272110a405
Jan Meinl <jan.f.meinl@web.de>
2026-05-16 20:46:17 +0200

    Refine Task05 in Flight03: handle multiple in-air declarations, enforce goal declaration checks, and adjust penalty logic.

--- Coordinates/JansScoring/flights/impl/03/tasks/Task05.cs
+++ Coordinates/JansScoring/flights/impl/03/tasks/Task05.cs
@@ -1,5 +1,6 @@
 using Coordinates;
 using System;
+using System.Collections.Generic;
 
 namespace JansScoring.flights.impl._03.tasks;
 
@@ -16,6 +17,15 @@ public class Task05 : TaskFON
 
     public override bool ScoringChecks(Track track, ref string comment)
     {
+        List<Declaration> declarations =
+            track.Declarations.FindAll(declaration => declaration.GoalNumber == DeclarationNumber());
+
+        if (declarations.Count == 0)
+        {
+            comment += $"Pilot has no declaration in goal number {DeclarationNumber()} | ";
+            return true;
+        }
+
         Declaration declaration = track.GetLatestDeclaration(DeclarationNumber());
         if (declaration == null)
         {
@@ -25,16 +35,40 @@ public class Task05 : TaskFON
 
         if (Math.Abs(declaration.DeclaredGoal.AltitudeBarometric - CoordinateHelpers.ConvertToMeter(2000)) > 0.1)
         {
-            comment += $"Declared altitude is not 2000m. ({declaration.DeclaredGoal.AltitudeBarometric}) | ";
+            comment += $"Declared altitude is not 2000ft. ({declaration.DeclaredGoal.AltitudeBarometric}) | ";
             declaration.DeclaredGoal.AltitudeBarometric = CoordinateHelpers.ConvertToMeter(2000);
         }
-        
-        var distanceBetweenDecAndDecPoint = CoordinateHelpers.Calculate2DDistanceHavercos(declaration.DeclaredGoal, declaration.PositionAtDeclaration);
+
+        var distanceBetweenDecAndDecPoint =
+            CoordinateHelpers.Calculate2DDistanceHavercos(declaration.DeclaredGoal, declaration.PositionAtDeclaration);
         if (distanceBetweenDecAndDecPoint < 3000)
         {
-            var penalty = DistanceViolationPenalties.CalculateAndFormatPenalty(distanceBetweenDecAndDecPoint, 3000, "TP");
-            comment += $"Declared goal is to close to declaration point. ({distanceBetweenDecAndDecPoint}m / 3000m) [{penalty}] | ";
+            var penalty =
+                DistanceViolationPenalties.CalculateAndFormatPenalty(distanceBetweenDecAndDecPoint, 3000, "TP");
+            comment +=
+                $"Declared goal is to close to declaration point. ({distanceBetweenDecAndDecPoint}m / 3000m) [{penalty}] | ";
         }
+
+        TrackHelpers.EstimateLaunchAndLandingTime(track, Flight.UseGPSAltitude(), out Coordinate launchpoint, out _);
+        int declarationsAfterEstimated = 0;
+        Declaration fistDeclarationInAir = null;
+        declarations.ForEach(declaration =>
+        {
+            if (declaration.PositionAtDeclaration.TimeStamp > launchpoint.TimeStamp)
+            {
+                if (fistDeclarationInAir == null)
+                    fistDeclarationInAir = declaration;
+                declarationsAfterEstimated++;
+            }
+        });
+        if (declarationsAfterEstimated > 1)
+        {
+            comment += "Pilot has maybe more than one declaration in the air | ";
+            track.Declarations.RemoveAll(declaration1 =>
+                declaration1.PositionAtDeclaration.TimeStamp > launchpoint.TimeStamp &&
+                declaration1 != fistDeclarationInAir);
+        }
+
         return false;
     }
 
