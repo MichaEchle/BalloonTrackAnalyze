19e085472800fb3e664ce0a591f8f8cf1ca12b14
MICHA ECHLE <micha_echle@keysight.com>
2026-08-05 19:42:51 +0200

    Add HNBC2026 project with initial setup and logging
    Added HNBC2026 project to the solution with references to Competition, Coordinates, and LoggingConnector. Configured Microsoft.Extensions.Logging.Console dependency. Introduced Flight1 class with logging and scoring logic skeleton. Added Program.cs to set up logging and invariant culture. Updated Coordinates.sln to include HNBC2026 and its build configurations.

---
+++ Coordinates/HNBC2026/Flight1.cs
@@ -0,0 +1,42 @@
+﻿using Competition;
+using Coordinates;
+using LoggingConnector;
+using Microsoft.Extensions.Logging;
+
+namespace HNBC2026;
+
+internal class Flight1
+{
+    private ILogger<Flight1> _logger = LogConnector.LoggerFactory.CreateLogger<Flight1>();
+    private double _separationAltitude = CoordinateHelpers.ConvertToMeter(2400);
+    private Flight _flight = Flight.GetInstance();
+
+
+    internal void ScoreFlight()
+    {
+        _logger.LogInformation("Scoring Flight 1");
+        _flight.FlightNumber = 1;
+        if (!_flight.ParseTrackFiles(@"C:\Users\micechle\Dropbox\2026-08-05 Horb Scoring\2026 Horb Tracks", true))
+        {
+            _logger.LogError("Error parsing track files");
+            return;
+        }
+        if (_flight.MapPilotNamesToTracks(@"C:\Users\micechle\Dropbox\2026-08-05 Horb Scoring"))
+        {
+            _logger.LogError("Error mapping pilot names to tracks");
+            return;
+        }
+        List<Track> tracks = [.. _flight.Tracks.OrderBy(x => x.Pilot.PilotNumber)];
+
+    }
+
+    private void Task1()
+    {
+        //TODO check launch timing
+        //TODO check distance launch to goals
+        //TODO check declaration to declared goals
+        //TODO check task order
+        //TODO check task timings
+        //TODO calculate result
+    }
+}
---
+++ Coordinates/HNBC2026/Program.cs
@@ -0,0 +1,16 @@
+﻿using LoggingConnector;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+        LogConnector.LoggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+        
+    }
+}
